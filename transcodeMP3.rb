81f874d4aab71f56a70b60f6a8ce3d77b827afab
Karthik Periagaram <dekonvoluted@gmail.com>
2016-02-27 11:10:17 -0800

    Verify flac library exists and create mp3 library if needed
    The script takes two arguments, the upstream flac library and the
    downstream mp3 library. It will ensure that the flac library exists, or
    raise an exception.
    
    If the mp3 library doesn't exist, it will create the full path. If the
    creation fails for some reason, an exception will be raised.

--- transcodeMP3.rb
+++ transcodeMP3.rb
@@ -4,6 +4,17 @@
 # Keep libraries in sync
 # Keep MP3 library file names FAT32-compatible
 
+require "fileutils"
+
+raise ArgumentError, "Too many arguments" unless ARGV.length == 2
+
+FLACDIR = ARGV.at 0
+raise ArgumentError, "FLAC library not found" unless Dir.exist? FLACDIR
+
+MP3DIR = ARGV.at 1
+FileUtils.mkpath MP3DIR unless Dir.exist? MP3DIR
+raise ArgumentError, "MP3 library not found" unless Dir.exist? MP3DIR
+
 # Create FAT32-safe file/directory names
 def getFAT32SafeName badName
     # Avoid strings longer than 240 characters (with .mp3 extension)
