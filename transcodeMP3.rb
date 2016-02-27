e7c4cf0ddc6799354ad404a14e4c6a26c11a4f35
Karthik Periagaram <dekonvoluted@gmail.com>
2016-02-27 11:21:23 -0800

    Separate out FAT32 safe name method into a separate file

--- transcodeMP3.rb
+++ transcodeMP3.rb
@@ -4,6 +4,8 @@
 # Keep libraries in sync
 # Keep MP3 library file names FAT32-compatible
 
+require_relative "fat32safename"
+
 require "fileutils"
 
 raise ArgumentError, "Too many arguments" unless ARGV.length == 2
@@ -14,28 +16,6 @@ raise ArgumentError, "FLAC library not found" unless Dir.exist? FLACDIR
 MP3DIR = ARGV.at 1
 FileUtils.mkpath MP3DIR unless Dir.exist? MP3DIR
 raise ArgumentError, "MP3 library not found" unless Dir.exist? MP3DIR
-
-# Create FAT32-safe file/directory names
-def getFAT32SafeName badName
-    # Avoid strings longer than 240 characters (with .mp3 extension)
-    safeName = badName.slice( 0, 236 )
-
-    # Allow only safe characters
-    badCharacters = /[^a-zA-Z0-9\.\-\ ]/
-    safeName.gsub! badCharacters, "_"
-
-    # Avoid leading/trailing spaces or dots
-    safeName.gsub! /^\s/, "_"
-    safeName.gsub! /\s$/, "_"
-    safeName.gsub! /^\./, "_"
-    safeName.gsub! /\.$/, "_"
-
-    # Avoid empty or "." file names
-    safeName = "empty" if safeName.empty?
-    safeName = "dot" if safeName == "."
-
-    return safeName
-end
 
 # Gather paths to all FLAC media in the flac library
 flacPaths = Dir.glob File.join "#{FLACDIR}", "**", "*.flac"
