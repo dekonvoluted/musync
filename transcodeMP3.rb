24e8e05ac9081b179c7db496986d8a3397f83e40
Karthik Periagaram <dekonvoluted@gmail.com>
2016-02-27 11:00:11 -0800

    Implement a simple set of tests for the FAT32 safe names
    This test compares the output of the FAT32-safe name generator when it
    is fed some test strings. If the output matches expectations, the test
    is passed. If any test fails, the input, output and expected output are
    printed out.
    
    Currently, the method passes all tests.

--- transcodeMP3.rb
+++ transcodeMP3.rb
@@ -26,3 +26,42 @@ def getFAT32SafeName badName
     return safeName
 end
 
+# Test safe FAT32 names function
+def testFAT32SafeNames
+    testStrings = Array.new
+    expectedResults = Array.new
+
+    # Test a simple string
+    testStrings.push     "This is a safe string"
+    expectedResults.push "This is a safe string"
+
+    # Test special characters
+    testStrings.push     "[\\/:*?\"<>|,.]&"
+    expectedResults.push "___________.__"
+
+    # Test long strings
+    testStrings.push "This is a string that is simply too long and will need to be truncated to fit within the 240 character limit if it is to be considered safe for the File Allocation Table 32 bit file system that was all the rage back in the day when such things were common"
+    expectedResults.push "This is a string that is simply too long and will need to be truncated to fit within the 240 character limit if it is to be considered safe for the File Allocation Table 32 bit file system that was all the rage back in the day when such"
+
+    # Test leading/trailing spaces or dots
+    testStrings.push     " leading space trailing space "
+    expectedResults.push "_leading space trailing space_"
+
+    testStrings.push ".leading dot trailing dot."
+    expectedResults.push "_leading dot trailing dot_"
+
+    # Compare expectations with actual output
+    testStrings.each_with_index do | value, index |
+        if getFAT32SafeName( value ) != expectedResults.at( index )
+            puts "Failed:"
+            puts "Input: #{value}"
+            puts "Expected: #{expectedResults.at( index )}"
+            puts "Actual: #{getFAT32SafeName( value )}"
+        else
+            puts "Passed."
+        end
+    end
+end
+
+testFAT32SafeNames
+
