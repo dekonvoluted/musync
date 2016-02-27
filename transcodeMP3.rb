c03f5a092271b0a06221f174eef9ffddce903e7e
Karthik Periagaram <dekonvoluted@gmail.com>
2016-02-27 10:58:51 -0800

    Add method to get FAT32-safe file and directory names
    This method is necessary as the original path may not be translatable to
    a FAT32-safe string.

--- transcodeMP3.rb
+++ transcodeMP3.rb
@@ -4,3 +4,25 @@
 # Keep libraries in sync
 # Keep MP3 library file names FAT32-compatible
 
+# Create FAT32-safe file/directory names
+def getFAT32SafeName badName
+    # Avoid strings longer than 240 characters (with .mp3 extension)
+    safeName = badName.slice( 0, 236 )
+
+    # Allow only safe characters
+    badCharacters = /[^a-zA-Z0-9\.\-\ ]/
+    safeName.gsub! badCharacters, "_"
+
+    # Avoid leading/trailing spaces or dots
+    safeName.gsub! /^\s/, "_"
+    safeName.gsub! /\s$/, "_"
+    safeName.gsub! /^\./, "_"
+    safeName.gsub! /\.$/, "_"
+
+    # Avoid empty or "." file names
+    safeName = "empty" if safeName.empty?
+    safeName = "dot" if safeName == "."
+
+    return safeName
+end
+
