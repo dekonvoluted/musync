228d4a92d21c5d6b86608e734f872f7e4b54fe98
Karthik Periagaram <dekonvoluted@gmail.com>
2016-03-13 14:55:49 -0700

    Derive a class for dealing with artwork
    This derived class is based on the mu file parent class and has a method
    for resizing and copying itself to a destination.

--- artfile.rb
+++ artfile.rb
@@ -1,20 +1,19 @@
+require_relative "mufile"
+
 require "shellwords"
 
-class ArtFile
-    def initialize path
-        # Require that the filename end with either JPG or PNG
-        raise "Invalid album art file name" unless [ ".jpg", ".jpeg", ".png" ].include? File.extname( path ).downcase
+class ArtFile < MuFile
 
-        @artPath = path
-    end
+    # Specify valid extensions
+    @valid_extensions = [ ".jpg", ".jpeg", ".png" ]
+
+    # Specify error message
+    @invalid_extension_message = "Invalid extension for artwork"
 
     # Write out resized artwork
     def resize destination, dimensions
         # Require dimensions to be in ImageMagick-compatible format
         raise "Invalid size for resized artwork" unless dimensions =~ /\d*x\d*/
-
-        # Require that artwork source file exist
-        raise "File not found" unless File.exist? @artPath
 
         # Check if convert exists
         raise "convert: Command not found" unless system( "which convert &> /dev/null" )
@@ -23,7 +22,7 @@ class ArtFile
         artworkResizeCommand = Array.new
         artworkResizeCommand.push "convert"
         artworkResizeCommand.push "-resize #{dimensions}"
-        artworkResizeCommand.push Shellwords.escape @artPath
+        artworkResizeCommand.push Shellwords.escape File.join( @base_directory, @relative_path )
         artworkResizeCommand.push Shellwords.escape destination
 
         raise "Resizing of artwork failed" unless system( artworkResizeCommand.join( " " ) )
