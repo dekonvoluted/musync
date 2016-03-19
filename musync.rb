cfb0c253d6a577960cc1dd6aefc3e719dc826825
Karthik Periagaram <dekonvoluted@gmail.com>
2016-03-19 11:18:04 -0700

    Create new executable frontend
    This replaces the old transcodeMP3 frontend.

---
+++ musync.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+# Transcode and sync a FLAC library to a portable MP3 library
+
+require_relative "library"
+
+require "fileutils"
+require "optparse"
+
+if __FILE__ == $0
+    optparse = OptionParser.new do | opts |
+        opts.banner = "Usage: #{$0} [FLAC library] [MP3 library]"
+
+        opts.on( "-h", "--help", "Display this help message" ) do
+            puts opts
+            exit 0
+        end
+    end
+
+    optparse.parse!
+
+    # Accept two arguments
+    raise ArgumentError, "Too many arguments" unless ARGV.length == 2
+
+    # Ensure upstream library exists
+    FLACDIR = ARGV.at 0
+    raise ArgumentError, "FLAC library not found" unless Dir.exist? FLACDIR
+    flacLibrary = Library.new FLACDIR
+
+    # Create downstream library if it doesn't exist
+    MP3DIR = ARGV.at 1
+    FileUtils.mkpath MP3DIR unless Dir.exist? MP3DIR
+    raise ArgumentError, "MP3 library not found" unless Dir.exist? MP3DIR
+
+    # Initiate sync
+    flacLibrary.sync_to MP3DIR
+end
+
+exit 0
+
