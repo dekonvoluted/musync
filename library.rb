ab116ee1487b323f2b9a8304792936420fa03a5b
Karthik Periagaram <dekonvoluted@gmail.com>
2016-03-29 23:49:02 -0700

    Simplify logic to work with missing artwork
    Since the hash containing the artwork for each relative path is
    initialized to an empty array if the relative path has no artwork, this
    section of the code gets much simpler.

--- library.rb
+++ library.rb
@@ -66,26 +66,24 @@ class Library
 
             # Resize and copy album artwork
             artFileArray = @artFiles[ relativePath ]
-            unless artFileArray.nil?
-                artFileArray.each do | artFile |
+            artFileArray.each do | artFile |
 
-                    # Skip already existing files
-                    downstreamArtFilePath = File.join downstreamBaseDirectory, artFile.safe_relative_path
-                    next if File.exist? downstreamArtFilePath
+                # Skip already existing files
+                downstreamArtFilePath = File.join downstreamBaseDirectory, artFile.safe_relative_path
+                next if File.exist? downstreamArtFilePath
 
-                    # Create directory if missing
-                    safeRelativeDirectory = File.dirname downstreamArtFilePath
-                    FileUtils.mkpath safeRelativeDirectory unless Dir.exist? safeRelativeDirectory
+                # Create directory if missing
+                safeRelativeDirectory = File.dirname downstreamArtFilePath
+                FileUtils.mkpath safeRelativeDirectory unless Dir.exist? safeRelativeDirectory
 
-                    puts "Syncing #{artFile.safe_relative_path}"
+                puts "Syncing #{artFile.safe_relative_path}"
 
-                    # Resize to destination
-                    artFile.resize downstreamArtFilePath, "300x300"
-                end
+                # Resize to destination
+                artFile.resize downstreamArtFilePath, "300x300"
             end
 
             # Designate destination album artwork
-            artFile = artFileArray.at( 0 ) unless artFileArray.nil? or artFileArray.size > 1
+            artFile = artFileArray.at( 0 ) unless artFileArray.empty? or artFileArray.size > 1
 
             mediaFileArray.each do | flacFile |
 
